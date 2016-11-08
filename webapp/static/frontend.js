65bd19284ae10ffc404fef0ea6e046edbd87acc2
Buck Baskin <mobile.wbaskin@gmail.com>
2016-11-08 15:44:20 -0500

    Form up and running

--- webapp/static/frontend.js
+++ webapp/static/frontend.js
@@ -1,7 +1,7 @@
 console.log('Hello World');
 
 function submitTrip() {
-  console.log("from?: "+$( "#trip-from" ).text);
-  console.log("to?: "+$( "#trip-to" ).text);
-  console.log("at?: "+$( "#trip-at" ).text);
+  console.log("from?: "+$( "#trip-from" )[0].value);
+  console.log("to?: "+$( "#trip-to" )[0].value);
+  console.log("at?: "+$( "#trip-at" )[0].value);
 }
