c96fc48ffc4b364e39a28249d59456ba8ec5c828
Buck Baskin <buckbaskin@google.com>
2016-11-10 15:32:36 -0500

    Add a search for trips, and render in template. Also Trip detail page

--- webapp/api.py
+++ webapp/api.py
@@ -1,6 +1,16 @@
 from webapp import server as router
 
-from flask import render_template, request
+from flask import render_template, request, redirect
+
+import psycopg2
+
+try:
+    conn = psycopg2.connect(dbname='postgres', user='postgres',
+        password='economicalchinchillacorndog', host='localhost')
+except:
+    print('No Database.')
+
+EXAMPLE_TRIP = ('special_trip_id', 'Leutner', 'Fribley', '3:00PM', 'specialuserid',)
 
 @router.route('/')
 def index():
@@ -15,12 +25,58 @@ def tripfinder():
         from_ = ''
         to_ = ''
         at_ = ''
+    prefer_friends = request.args.get('friends')
     
-    # get the information from the page load. If it doesn't come, skip this.
+    try:
+        cur = conn.cursor()
+
+        cur.execute("""SELECT * FROM trips""")
+
+        trips = cur.fetchmany(3)
+    except:
+        trips = []
+        print('Database requets failed.')
     
     if not from_ == '' and not to_ == '' and not at_ == '':
         message = 'The database results go here. We probably want to template a list.'
     else:
         message = 'Can you provide more information?'
+        trips = []
+
+    if trips == []:
+        trips = [EXAMPLE_TRIP]
     
-    return render_template('find_trip.html', from_=from_, to_=to_, at_=at_, message=message)
+    return render_template('find_trip.html', 
+        from_=from_, to_=to_, at_=at_, message=message, 
+        friend_trips=[], trips=trips)
+
+def expandIdentifier(shortIdentifier):
+    return shortIdentifier
+
+def compressIdentifier(longIdentifier):
+    return longIdentifier
+
+@router.route('/t/<shorttripid>/join', methods=['GET'])
+def joinTripPage(shorttripid):
+    return redirect('/t/%s' % (shorttripid,))
+
+@router.route('/t/<shorttripid>', methods=['GET'])
+def tripDetailPage(shorttripid):
+    long_id = expandIdentifier(shorttripid)
+
+    try:
+        cur = conn.cursor()
+
+        cur.execute('''SELECT * FROM trips WHERE id = long_id''')
+
+        trip = cur.fetchmany(1)
+    except:
+        trip = []
+
+    if len(trip) <= 0:
+        if shorttripid == 'special_trip_id':
+            return render_template('trip_detail.html', trip=EXAMPLE_TRIP)
+        # no result
+        return redirect('/trip')
+    else:
+        return render_template('trip_detail.html', trip=trip)
