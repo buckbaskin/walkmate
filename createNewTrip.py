c25216b60b56bd6641b11b5e30a39321beea3f9f
Robert Wagner <raw141@case.edu>
2016-11-28 20:04:21 -0500

    Bobby's first commit

---
+++ createNewTrip.py
@@ -0,0 +1,34 @@
+import psycopg2
+import uuid
+import datetime
+
+# make a connection
+try:
+    conn = psycopg2.connect(database='postgres', user='postgres', password='economicalchinchillacorndog', host='localhost')
+    cur = conn.cursor()
+except:
+    print('Database Connection Failed')
+    raise
+
+def createNewTrip(uuuid,start_destination,end_destination,start_time):
+    tripid = uuid.uuid.uuid4().hex
+    errorflag = False
+    try:
+        query1 = "INSERT INTO TRIPS (tripid, start_destination, end_destination, start_time, number_partipants)"
+        data1 = (tripid, start_destination, end_destination, start_time, 1)
+        cur.execute(query1,data1)
+    except:
+        print('Database insertion error, most likely not uuid collision')
+        errorflag = True
+        raise
+    try:
+        query2 = "INSERT INTO ONTRIP (tripid, uuuid)"
+        data2 = (tripid, uuid)
+    except:    
+        print('Database insertion error, most likely not uuid collision')
+        errorflag = True
+        raise
+    if not errorflag:
+        cur.commit()
+        cur.close()
+    
---
+++ createNewUser.py
@@ -0,0 +1,36 @@
+import psycopg2
+import uuid
+import datetime
+
+# make a connection
+try:
+    conn = psycopg2.connect(database='postgres', user='postgres', password='economicalchinchillacorndog', host='localhost')
+    cur = conn.cursor()
+except:
+    print('Database Connection Failed')
+    raise
+
+def createNewUser(username, hashed_password, first_name, last_name):
+    try:
+        uuuid = uuid.uuid.uuid4().hex
+        date_joined = datetime.datetime.utcnow()
+        query = "INSERT INTO USERS (uuuid, username, hashed_password, first_name, last_name, date_joined) VALUES (%s, %s, %s, %s, %s, %s);"
+        data = (uuuid, username, hashed_password, first_name, last_name, date_joined)
+        cur.execute(query,data)
+    except:
+        print('Possible uuid error, trying again')
+        try:
+            uuuid = uuid.uuid.uuid4().hex
+            query = "INSERT INTO USERS (uuuid, username, hashed_password, first_name, last_name, date_joined) VALUES (%s, %s, %s, %s, %s, %s);"
+            data = (uuuid, username, hashed_password, first_name, last_name, date_joined)
+            cur.execute(query,data)
+        except:
+            print('Database insertion error, most likely not uuid collision')
+            raise
+    
+    cur.commit()
+    cur.close()
+
+
+if __name__ == '__main__':
+    main()
