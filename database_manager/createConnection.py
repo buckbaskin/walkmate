680f815720bd3abee99f3af406672e5de403a450
Robert Wagner <raw141@case.edu>
2016-12-09 18:04:35 -0500

    cursor upgrades

---
+++ database_manager/createConnection.py
@@ -0,0 +1,9 @@
+import psycopg2
+def createNewConnection():
+	try:
+    	conn = psycopg2.connect(database='postgres', user='postgres', password='economicalchinchillacorndog', host='localhost')
+    	cur = conn.cursor()
+	except:
+    	print('Database Connection Failed')
+    	raise
+    return cur
