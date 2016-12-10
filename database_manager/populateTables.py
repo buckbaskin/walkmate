54cb84729b3ea4374f262afcfb18484b0e7cb1a6
Robert Wagner <raw141@case.edu>
2016-12-10 02:32:06 -0500

    Added trial users, edit your passwords

--- database_manager/populateTables.py
+++ database_manager/populateTables.py
@@ -1,5 +1,7 @@
 import pyscopg2
 import populateDestinations
+import populateUsers
 def populateAll(cur):
 	populateDestinations.populateDestinations(cur)
+	populateUsers.populateUsers(cur)
 	cur.commit()
